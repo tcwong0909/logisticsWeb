0525a1fe4cf76a5cfebbdfa57f3ded29446c8a59
tcwong <1394031559@qq.com>
2019-05-24 15:10:08 +0800

    驾驶员搜索完成

--- src/components/driver/Driver.vue
+++ src/components/driver/Driver.vue
@@ -1,6 +1,54 @@
 <template>
   <div>
-    <div style="margin-top: 20px" >
+
+    <div>
+      <el-card  body-style="padding:10px" shadow="never" style="display: flex;align-items: center">
+        <el-form>
+          <tr>
+            <td>
+              <el-input placeholder="请输入内容" suffix-icon="el-icon-search" v-model="searchDriver.name">
+                <template slot="prepend">司机姓名</template>
+              </el-input>
+            </td>
+            <td style="padding-left: 10px">
+              <el-tag>所属车队</el-tag>
+              <template >
+                <el-select
+                  v-model="searchDriver.fkTeamid"
+                  @visible-change="selectChanges"
+                  placeholder="请选择">
+                  <el-option label="全部" :value="''"></el-option>
+                  <el-option
+                    v-for="truckTeam in truckTeams"
+                    :key="truckTeam.teamid"
+                    :label="truckTeam.teamname"
+                    :value="truckTeam.teamid">
+                  </el-option>
+                </el-select>
+              </template>
+            </td>
+            <td style="padding-left: 10px">
+              <el-tag>工作状态</el-tag>
+              <template>
+                <el-select v-model="searchDriver.state" placeholder="请选择">
+                  <el-option label="全部" :value="''"></el-option>
+                  <el-option label="承运中" :value="1"></el-option>
+                  <el-option label="空闲" :value="2"></el-option>
+                </el-select>
+              </template>
+            </td>
+            <td style="padding-left: 10px">
+              <el-button type="danger" icon="el-icon-full-screen" @click="resetSearch">重置</el-button>
+            </td>
+            <td style="padding-left: 10px">
+              <el-button type="primary" icon="el-icon-search" @click="doSearch">搜索</el-button>
+            </td>
+          </tr>
+        </el-form>
+      </el-card>
+    </div>
+    <el-card shadow="never" body-style="padding:0;padding-top:1px">
+    <div >
       <el-button type="primary" icon="el-icon-plus" size="small" @click="showDialog('add')">添加驾驶员</el-button>
       <el-button type="danger" icon="el-icon-minus" size="small" @click="multiDelete" :disabled="multipleSelection.length===0">批量删除</el-button>
     </div>
@@ -157,6 +205,7 @@
         :total="total">
       </el-pagination>
     </div>
+    </el-card>
   </div>
 </template>
 
@@ -173,6 +222,11 @@
         currentPage:1,
         dialogFormVisible:false,
         dialogTitle:'',
+        searchDriver:{
+          name:'',
+          fkTeamid:'',
+          state:''
+        },
         driver:{
           name:'',
           sex:null,
@@ -183,13 +237,32 @@
           remark:'',
           fkTeamid:null
         },
-        drivers:[]
+        drivers:[],
+      }
+    },
+    watch:{
+      searchDriver:{
+        handler(){
+          this.doSearch();
+        },
+        deep:true,
+        immediate:true
       }
     },
     mounted(){
       this.loaddrivers();
     },
     methods:{
+      selectChanges(){
+        this.loadTruckTeams();
+      },
+      resetSearch(){
+        this.searchDriver={
+          name:'',
+          fkTeamid:'',
+          state:''
+        }
+      },
       initdriver(){
         this.driver={
           name:'',
@@ -224,8 +297,20 @@
         })
       },
       loaddrivers(){
-        this.getRequest("/driver/getAllByPage?page="+this.currentPage+"&size="+this.pageSize).then(res=>{
+        this.postRequest("/driver/getAllByPage?page="+this.currentPage+"&size="+this.pageSize+ "&name="+
+          this.searchDriver.name+ "&fkTeamid="+this.searchDriver.fkTeamid+"&state="+this.searchDriver.state).then(res=>{
           if (res){
+            this.drivers=res.data.data;
+            this.total = res.data.total;
+          }
+        })
+      },
+      doSearch(){
+        let page = 1;
+        let size = 10;
+        this.postRequest("/driver/getAllByPage?page="+page+"&size="+size+"&name="+this.searchDriver.name+
+        "&fkTeamid="+this.searchDriver.fkTeamid+"&state="+this.searchDriver.state).then(res=>{
+          if (res) {
             this.drivers=res.data.data;
             this.total = res.data.total;
           }
@@ -277,7 +362,7 @@
 
       },
       loadTruckTeams(){
-        this.getRequest("/truckTeam/getAllByPage").then(res=>{
+        this.postRequest("/truckTeam/getAllByPage").then(res=>{
           if (res){
             this.truckTeams=res.data.data;
           }
